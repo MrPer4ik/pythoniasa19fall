a23a628f8425e964b0979abd02187bbb35971442
Alexander Khomych <arkhomych.lit@gmail.com>
2019-09-27 09:56:39 +0300

    Completed assignment02
    Also tasks from assignment01 were wrapped in functions.

--- assignment02a.py
+++ assignment02a.py
@@ -10,7 +10,28 @@ True
 """
 
 def poem():
-    return ''
+    result = ''
+    base = [["the house that Jack built", "lay in"],
+            ["the malt", "ate"],
+            ["the rat", "killed"],
+            ["the cat", 'worried'],
+            ["the dog", "tossed"],
+            ["the cow with the crumpled horn", "milked"],
+            ["the maiden all forlorn", "kissed"],
+            ["the man all tattered and torn", 'married'],
+            ["the priest all shaven and shorn", "waked"],
+            ["the cock that crowed in the morn", "kept"],
+            ["the farmer sowing his corn", 'is']]
+
+    for i in range(len(base)):
+        for j in range(i, 0, -1):
+            if j == i:
+                result += f'This is {base[j][0]}'
+            else:
+                result += f'That {base[j][1]} {base[j][0]}'
+            result += ',\n' if j > 1 else '\n'
+        result += f'This is {base[i][i]}.\n\n' if i == 0 else f'That {base[0][1]} {base[0][0]}.\n\n'
+    return result[:-1]
 
 
 if __name__ == '__main__':
